dd85551e2c1fe7e6292264d155a423818cb5a25a
Mateusz Grzonka <alpinus4@gmail.com>
2023-03-08 18:39:34 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use eframe::egui;
+
+enum Message {
+    Text(String),
+    File {filename: String, data: Vec<u8>}
+}
+
+fn main() {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native("My egui App", native_options, Box::new(|cc| Box::new(CommunicatorApp::new(cc))));
+}
+
+#[derive(Default)]
+struct CommunicatorApp {}
+
+impl CommunicatorApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
+        // Restore app state using cc.storage (requires the "persistence" feature).
+        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+        // for e.g. egui::PaintCallback.
+        Self::default()
+    }
+}
+
+impl eframe::App for CommunicatorApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello World!");
+        });
+    }
+}
